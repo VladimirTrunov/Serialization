b035040b44ba030c58c13a33f84b5132e0eab62b
V-vltru <iv.vovatrunov__37@mail.ru>
2017-11-01 11:26:55 +0300

    Added testing of content definition

--- XMLSerialization/XMLDeserTesting/DeserializationTesting.cs
+++ XMLSerialization/XMLDeserTesting/DeserializationTesting.cs
@@ -5,7 +5,7 @@ using XMLSerializationLibrary;
 namespace XMLDeserTesting
 {
     [TestClass]
-    public class UnitTest1
+    public class DeserializationTesting
     {
         [TestMethod]
         public void TestGetMainTag()
@@ -26,6 +26,26 @@ namespace XMLDeserTesting
             string fact = deserialization.GetMainTag(deserialization.XML_String);
 
             Assert.AreEqual(expected, fact);
+        }
+        [TestMethod]
+        public void TestContentDefinition()
+        {
+            string xml = "<a>123</a>";
+            TagContent content = TagContent.Content;
+            _XMLDeserialization deserialization = new _XMLDeserialization(xml);
+            TagContent factContent = deserialization.DefineTagContent(deserialization.XML_String);
+
+            Assert.AreEqual(content, factContent);
+        }
+        [TestMethod]
+        public void TestContentDefinition2()
+        {
+            string xml = "<a><b>123</b></a>";
+            TagContent content = TagContent.Tree;
+            _XMLDeserialization deserialization = new _XMLDeserialization(xml);
+            TagContent factContent = deserialization.DefineTagContent(deserialization.XML_String);
+
+            Assert.AreEqual(content, factContent);
         }
     }
 }
