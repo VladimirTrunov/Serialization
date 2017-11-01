451ab5078cb37d9ee97aeb22e3183b86dba8042e
V-vltru <iv.vovatrunov__37@mail.ru>
2017-11-01 11:58:14 +0300

    Added some more unit tests

--- XMLSerialization/XMLDeserTesting/DeserializationTesting.cs
+++ XMLSerialization/XMLDeserTesting/DeserializationTesting.cs
@@ -47,5 +47,63 @@ namespace XMLDeserTesting
 
             Assert.AreEqual(content, factContent);
         }
+
+        [TestMethod]
+        public void TestGettingContent()
+        {
+            string xml = "<a>123</a>";
+            string expected = "123";
+            string expectedOutStr = "";
+            _XMLDeserialization deserialization = new _XMLDeserialization(xml);
+            string outStr;
+
+            string fact = deserialization.GetContent(deserialization.XML_String, out outStr);
+
+            Assert.AreEqual(expected, fact);
+            Assert.AreEqual(expectedOutStr, outStr);
+        }
+
+        [TestMethod]
+        public void TestGettingContent2()
+        {
+            string xml = "<a type = \"open\">123</a><b>1</b>";
+            string expected = "123";
+            string expectedOutStr = "<b>1</b>";
+            _XMLDeserialization deserialization = new _XMLDeserialization(xml);
+            string outStr;
+
+            string fact = deserialization.GetContent(deserialization.XML_String, out outStr);
+
+            Assert.AreEqual(expected, fact);
+            Assert.AreEqual(expectedOutStr, outStr);
+        }
+
+        [TestMethod]
+        public void TestIsClosingTagNext()
+        {
+            string xml = "</a><b>1</b>";
+            bool expected = true;
+            _XMLDeserialization deserialization = new _XMLDeserialization(xml);
+
+            bool fact = deserialization.isClosingTagNext("a", deserialization.XML_String);
+            Assert.AreEqual(expected, fact);
+
+            fact = deserialization.isClosingTagNext("b", deserialization.XML_String);
+            Assert.AreNotEqual(expected, fact);
+        }
+
+        [TestMethod]
+        public void TestIsClosingTagNext2()
+        {
+            string xml = "<a><b>1</b>";
+            bool expected = false;
+            _XMLDeserialization deserialization = new _XMLDeserialization(xml);
+
+            bool fact = deserialization.isClosingTagNext("a", deserialization.XML_String);
+            Assert.AreEqual(expected, fact, "Attempt 1");
+
+            fact = deserialization.isClosingTagNext("b", deserialization.XML_String);
+            Assert.AreEqual(expected, fact, "Attempt 2");
+        }
     }
 }
